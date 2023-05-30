aa29b9b884c74db2dcaf0bef57800f82f6c466f1
Joy Serquiña <stepjoyous@gmail.com>
2023-05-29 22:12:28 -0700

    Added footer nav links and basic styles.

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -10,6 +10,32 @@ const Footer = () => {
         href={`https://www.joyserquina.com`}>
         Joy of All Trades &copy; 2019
       </Link>
+      <nav>
+        <Link
+          className={styles.navLink}
+          href='/portfolio'
+          name='Portolio'>
+          Portfolio
+        </Link>
+        <Link
+          className={styles.navLink}
+          href='/about'
+          name='About'>
+          About
+        </Link>
+        <Link
+          className={styles.navLink}
+          href='/contact'
+          name='Contact'>
+          Contact
+        </Link>
+        <Link
+          className={styles.navLink}
+          href='/resume'
+          name='Resume'>
+          Resume
+        </Link>
+      </nav>
     </footer>
   )
 }
